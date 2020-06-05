a0d135d98053a0e4e86f880939f8e092f3fe1534
Bryan dos Santos Gomes <bryandossantosg8@gmail.com>
2020-06-05 14:33:46 -0300

    UI finished

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,9 +1,51 @@
 import React from 'react';
-import { View } from 'react-native';
-
+import { View , StyleSheet, TouchableOpacity, Image, Text, SafeAreaView } from 'react-native';
+import { Feather as Icon, FontAwesome as FIcon } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native';
+import { RectButton} from 'react-native-gesture-handler';
 const Detail = () => {
+
+  const navigation = useNavigation()
+
+  function handleNavBack () {
+    navigation.goBack();
+  }
+
   return (
-    <View/>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+          <TouchableOpacity onPress={handleNavBack}>
+            <Icon 
+            name="arrow-left"
+            size={20}
+            color="#34cb79"
+            />
+          </TouchableOpacity>
+
+          <Image style={styles.pointImage} source={{ uri: 'https://i.picsum.photos/id/163/400/400.jpg'}}/>
+
+          <Text style={styles.pointName}> Mercaão do taobao</Text>
+          <Text style={styles.pointItems}>Oléos de Cozinha, Pilhas e Baterias</Text>
+
+          <View style={styles.address}>
+
+            <Text style={styles.addressTitle}>Endereço</Text>
+            <Text style={styles.addressContent}>Belo Horizonte, MG</Text>
+
+          </View>
+      </View>
+    
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={ ()=> {} }>
+          <FIcon name="whatsapp" size={20} color="#FFF" />
+          <Text style={styles.buttonText}>whatsapp</Text>
+        </RectButton>
+        <RectButton style={styles.button} onPress={ ()=> {} }>
+          <Icon name="mail" size={20} color="#FFF" />
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -11,76 +53,76 @@ export default Detail;
 
 
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     padding: 32,
-//     paddingTop: 20,
-//   },
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+    paddingTop: 20,
+  },
 
-//   pointImage: {
-//     width: '100%',
-//     height: 120,
-//     resizeMode: 'cover',
-//     borderRadius: 10,
-//     marginTop: 32,
-//   },
+  pointImage: {
+    width: '100%',
+    height: 120,
+    resizeMode: 'cover',
+    borderRadius: 10,
+    marginTop: 32,
+  },
 
-//   pointName: {
-//     color: '#322153',
-//     fontSize: 28,
-//     fontFamily: 'Ubuntu_700Bold',
-//     marginTop: 24,
-//   },
+  pointName: {
+    color: '#322153',
+    fontSize: 28,
+    fontFamily: 'Ubuntu_700Bold',
+    marginTop: 24,
+  },
 
-//   pointItems: {
-//     fontFamily: 'Roboto_400Regular',
-//     fontSize: 16,
-//     lineHeight: 24,
-//     marginTop: 8,
-//     color: '#6C6C80'
-//   },
+  pointItems: {
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 16,
+    lineHeight: 24,
+    marginTop: 8,
+    color: '#6C6C80'
+  },
 
-//   address: {
-//     marginTop: 32,
-//   },
+  address: {
+    marginTop: 32,
+  },
   
-//   addressTitle: {
-//     color: '#322153',
-//     fontFamily: 'Roboto_500Medium',
-//     fontSize: 16,
-//   },
+  addressTitle: {
+    color: '#322153',
+    fontFamily: 'Roboto_500Medium',
+    fontSize: 16,
+  },
 
-//   addressContent: {
-//     fontFamily: 'Roboto_400Regular',
-//     lineHeight: 24,
-//     marginTop: 8,
-//     color: '#6C6C80'
-//   },
+  addressContent: {
+    fontFamily: 'Roboto_400Regular',
+    lineHeight: 24,
+    marginTop: 8,
+    color: '#6C6C80'
+  },
 
-//   footer: {
-//     borderTopWidth: StyleSheet.hairlineWidth,
-//     borderColor: '#999',
-//     paddingVertical: 20,
-//     paddingHorizontal: 32,
-//     flexDirection: 'row',
-//     justifyContent: 'space-between'
-//   },
+  footer: {
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderColor: '#999',
+    paddingVertical: 20,
+    paddingHorizontal: 32,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
   
-//   button: {
-//     width: '48%',
-//     backgroundColor: '#34CB79',
-//     borderRadius: 10,
-//     height: 50,
-//     flexDirection: 'row',
-//     justifyContent: 'center',
-//     alignItems: 'center'
-//   },
+  button: {
+    width: '48%',
+    backgroundColor: '#34CB79',
+    borderRadius: 10,
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
 
-//   buttonText: {
-//     marginLeft: 8,
-//     color: '#FFF',
-//     fontSize: 16,
-//     fontFamily: 'Roboto_500Medium',
-//   },
-// });
+  buttonText: {
+    marginLeft: 8,
+    color: '#FFF',
+    fontSize: 16,
+    fontFamily: 'Roboto_500Medium',
+  },
+});
